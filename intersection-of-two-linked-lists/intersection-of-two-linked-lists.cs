f89b022d6e399916740deea141b072d1a827c29e
Akash <akashcsemu@gmail.com>
2021-06-16 08:00:45 +0600

    Time: 132 ms (68.74%), Space: 36.2 MB (68.87%) - LeetHub

---
+++ intersection-of-two-linked-lists/intersection-of-two-linked-lists.cs
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode GetIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode a = headA;
+        ListNode b = headB;
+        
+        while(a!=b){
+            a = a==null ? headB : a.next;
+            b = b==null ? headA : b.next;
+        }
+        return a;
+    }
+}
