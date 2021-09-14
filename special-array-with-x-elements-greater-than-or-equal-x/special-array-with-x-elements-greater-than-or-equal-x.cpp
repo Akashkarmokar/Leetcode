10a450a83bd829cc01eb770f04296754558544e2
Akash <akashcsemu@gmail.com>
2021-09-14 23:25:54 +0600

    Time: 3 ms (61.84%), Space: 8.4 MB (14.23%) - LeetHub

---
+++ special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int specialArray(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        for(int i=1;i<=nums.size();i++){
+            auto it = lower_bound(nums.begin(),nums.end(),i);
+            if(distance(it,nums.end())==i){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
