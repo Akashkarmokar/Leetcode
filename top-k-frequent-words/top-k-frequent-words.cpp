b1386ae7ced2654806cf884ffbbcfe54795cd1d1
Akash <akashcsemu@gmail.com>
2022-01-01 20:05:38 +0600

    Time: 21 ms (10.26%), Space: 12.6 MB (74.39%) - LeetHub

---
+++ top-k-frequent-words/top-k-frequent-words.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        unordered_map<string,int> ump;
+        for(int i=0;i<(int)words.size();i++){
+            ump[words[i]]++;
+        }
+        
+        // custom comparator function
+        auto comp = [](pair<int,string>& a, pair<int,string>& b){
+            if(a.first != b.first){
+                return a.first > b.first;
+            }else{
+                return a.second < b.second;
+            }
+
+        };
+        
+        priority_queue<pair<int,string>, vector<pair<int,string>>, decltype(comp) > minPQ(comp);
+        for(auto it: ump){
+            int freq = it.second;
+            string s = it.first;
+            minPQ.push({freq,s});
+            if(minPQ.size()>k){
+                minPQ.pop();
+            }
+        }
+        vector<string> ans;
+        while(!minPQ.empty()){
+            ans.push_back(minPQ.top().second);
+            minPQ.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
