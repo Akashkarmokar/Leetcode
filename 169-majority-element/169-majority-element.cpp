c71dadbd8de9ae263d830aa3ae8743ab291cc5be
Akash <akashcsemu@gmail.com>
2022-04-07 23:41:48 +0600

    Time: 32 ms (30.02%), Space: 19.5 MB (81.37%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,7 +1,34 @@
 class Solution {
 public:
+    int getFreq(vector<int>& a,int lo,int hi,int targetValue){
+        int count = 0;
+        for(int i=lo;i<=hi;i++){
+            if(a[i] == targetValue){
+                count++;
+            }
+        }
+        return count;
+    }
+public:
+    int getMajorElement(vector<int>& a,int lo,int hi){
+        if(lo==hi){
+            return a[lo];
+        }
+        int mid = lo + (hi-lo)/2;
+        int leftMajor = getMajorElement(a,lo,mid);
+        int rightMajor = getMajorElement(a,mid+1,hi);
+        
+        if(leftMajor == rightMajor){
+            return leftMajor;
+        }
+        
+        int leftFreq = getFreq(a,lo,mid,leftMajor);
+        int rightFreq = getFreq(a,mid+1,hi,rightMajor);
+        
+        return leftFreq > rightFreq ? leftMajor : rightMajor;
+        
+    }
     int majorityElement(vector<int>& nums) {
-       sort(nums.begin(),nums.end());
-        return nums[nums.size()/2];
+        return getMajorElement(nums,0,nums.size()-1);
     }
 };
