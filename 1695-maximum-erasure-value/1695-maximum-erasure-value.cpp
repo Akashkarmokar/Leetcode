f1dd31b56a40eccbdbaa124672d585cb7ac0f5ac
Akash <akashcsemu@gmail.com>
2022-06-12 08:28:18 +0600

    Time: 216 ms (95.38%), Space: 89.2 MB (97.81%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -1,19 +1,19 @@
 /**
     Time Complexity: O(N)
-    Space Complexity: O(N)
+    Space Complexity: O(1)
 */
 class Solution {
 public:
     int maximumUniqueSubarray(vector<int>& nums) {
         int n = nums.size(), curSum = 0, ans= 0, l = 0, r = 0;
-        unordered_set<int> s;
+        bool freq[10001]{false};
         while(l<=r && r<n){
-            while(s.find(nums[r]) != s.end()){
+            while(freq[nums[r]]){
                 curSum-=nums[l];
-                s.erase(nums[l++]);
+                freq[nums[l++]] = false;
             }
             curSum+=nums[r];
-            s.insert(nums[r++]);
+            freq[nums[r++]] = true;
             ans = max(ans,curSum);
         }
         return ans;
